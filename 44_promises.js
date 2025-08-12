2c33c5ba7a069d232ca4a618b15776817709edec
Mutee Ur Rehman <muteestacks@gmail.com>
2025-08-12 20:58:20 +0500

    Update 44_promises.js

--- 44_promises.js
+++ 44_promises.js
@@ -34,3 +34,65 @@ new Promise(function(resolve , reject){
 }).then(function(){
     console.log('promise completed')
 })
+
+
+
+// what to do if we recieve data from the network how can i pass data then
+const promiseThree = new Promise(function(resolve , reject){
+    // for ex a function did alot of work and recieve data from the network so
+    setTimeout(function(){
+        // in most of cases it is object inside resolve
+        resolve({username: "Mutee Ur Rehman" , email: "mutee@gamil.com"}) 
+    } , 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user)
+})
+
+
+let promiseFour = new Promise(function(resolve , reject){
+
+    setTimeout(function(){
+        let error = false  // change it to see changes 
+        if(!error){
+          resolve({username: "Mutee Ur Rehman" , password:'123'})
+        }
+        else{
+            reject("ERROR")
+        }
+         } , 1000)
+})
+
+
+
+// if we want a value from the object then we will return value from first then and use second then for that value this is also called chaining
+// for clean code we write .then and .catch on new line
+promiseFour
+.then((user)=>{
+    console.log(user)
+    return user.username
+})
+.then((username)=>{
+    console.log(username)
+})
+.catch((error)=>{console.log(error)})
+.finally(()=> console.log('This promise is either resolved or rejected'))   //you can say that .finally is default and will always run
+
+
+// in databse operations people sometime use **ayns await** instead of .then as well to handle promisis there is no performance issue 
+
+
+let promiseFive = new Promise(function(resolve , reject){
+    setTimeout(function(){
+        let error2 = false
+        if(!error2){
+            resolve({name: 'Javascript' , code: '.js'})
+        }
+        else{
+            reject('Error! You got an error')
+        }
+    },1000)
+})
+
+// async usually wait for the work to be done it the work is done then it continues else it gives an error
