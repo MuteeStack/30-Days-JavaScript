ce688168cc3ed9db7024568374d209fce195b7cf
Mutee Ur Rehman <muteestacks@gmail.com>
2025-08-23 11:24:15 +0500

    Update 51_Staticprop.js

--- 51_Staticprop.js
+++ 51_Staticprop.js
@@ -1,1 +1,36 @@
+class userData {
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`Username is ${this.username}`)
+    }
+
+   static createId(){   //when we use static it dont give access to any instance
+        return `123`
+    }
+
+}
+
+let user1 = new userData('Mutee Ur Rehman')
+
+// console.log(user1.createId())  
+
+
+class teacher extends userData{
+    constructor(username , email){
+        super(username)
+        this.email = email
+    }
+      printData(){
+        console.log(`Username is ${this.username} , Email is ${this.email}`)
+    }
+
+}
+
+const user2 = new teacher('mutee' , 'mutee@company.com')
+
+// user2.printData()
+// user2.logMe()
+user2.createId()  //this will not work sice we are using static 
 
